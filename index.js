f0a5d5a6130987a5715925b41fea5e33b85c3019
Sarah Whalley <s.whalley1130@gmail.com>
2023-01-30 19:44:55 -0700

    Completes lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+function saturdayFun (activity = "roller-skate")
+{
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(activity="go to the office")
+{
+    return `This Monday, I will ${activity}.`;
+}
+
+// function wrapAdjective(sym, adjective)
+// {
+//     return `You are a ${sym}${adjective}${sym}!`;
+// }
+
+function wrapAdjective(flair="*")
+{
+    return function(adjective="special")
+    {
+        return `You are ${flair}${adjective}${flair}!`;
+    }
+}
